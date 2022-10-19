0f036d46db799631b629d956d19e569f44abfefc
victor lage <victor.lage@sptech.school>
2022-10-19 19:53:04 -0300

    pleno

--- BD/scriptPy.sql
+++ BD/scriptPy.sql
@@ -32,7 +32,7 @@ SO VARCHAR(45),
 Maquina VARCHAR(45),
 Processador VARCHAR(45),
 Disco VARCHAR(45),
-VelocidadeRam INT,
+VelocidadeRam VARCHAR(10),
 fkEmpresa Int,
 FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
 );
@@ -75,32 +75,53 @@ INSERT INTO Usuario VALUES (null,"Pedro Neto",null,"pedro.cordeironeto@sptech.sc
                                   (null,"Luigi Ceolin",null,"luigi.ceolin@sptech.school","123",1,NULL),
                                   (null, "Adm EmpTeste",null, "admempteste@gmail.com",'123', 2, 1),
                                   (null, "Fun EmpTeste",null, "funempteste@gmail.com",'123', 3, 1);
-INSERT INTO Torre (idTorre) values  (101);
+INSERT INTO Torre (idTorre, fkEmpresa) values  (101,1),
+                                               (102,1),
+                                               (103,1),
+                                               (104,1),
+                                               (105,1);
 INSERT INTO Componente VALUES (0, null, null),
-							   (2, 'Processadores_qtd','psutil.cpu_count(logical=True)'),
+							   (1, 'processadores_qtd','psutil.cpu_count(logical=True)'),
+                               (2, 'processadores_nucleo_porcentagem','psutil.cpu_percent(interval = 1, percpu = True)'),
 							   (3, 'ram_total','round((psutil.virtual_memory() [0] / 10**9), 4)'),
                                (4, 'ram_uso', 'round((psutil.virtual_memory() [3] / 10**9), 4)'),
                                (5,'ram_uso_porcentagem','round((psutil.virtual_memory() [2]), 1)'),
-                               (6,'disco_total','round((psutil.disk_usage("C:\\\\")[0] / 10**12), 3)'),
-                               (7,'disco_uso','round((psutil.disk_usage("C:\\\\")[1] / 10**12), 3)'),
-                               (8,'disco_livre','round((psutil.disk_usage("C:\\\\")[2] / 10**12), 3)'),
-                               (9,'disco_uso_porcentagem','psutil.disk_usage("C:\\\\")[3]'),
+                               (6,'disco_total','round((psutil.disk_usage("/")[0] / 10**12), 3)'),
+                               (7,'disco_uso','round((psutil.disk_usage("/")[1] / 10**12), 3)'),
+                               (8,'disco_livre','round((psutil.disk_usage("/")[2] / 10**12), 3)'),
+                               (9,'disco_uso_porcentagem','psutil.disk_usage("/")[3]'),
                                (10,'pacotes_enviados','round((psutil.net_io_counters(pernic=False, nowrap=True) [2] / 1024), 2)'),
-                               (11,'pacotes_recebidos','round((psutil.net_io_counters(pernic=False, nowrap=True) [3] / 1024), 2)');
--- INSERT INTO Torre_Componente (fkTorre,fkComponente) values  (101, 2),
--- 	   													         (101, 3),
---                                                               (101, 4),
---                                                               (101, 5),
---                                                               (101, 6),
---                                                               (101, 7),
---                                                               (101, 8),
---                                                               (101, 9),
---                                                               (101, 10),
---                                                               (101, 11);
+                               (11,'pacotes_recebidos','round((psutil.net_io_counters(pernic=False, nowrap=True) [3] / 1024), 2)'),
+                               (12,'pacotes_perdidos_porcentagem','[pacotes_enviados, pacotes_recebidos]');
+INSERT INTO Torre_Componente (fkTorre,fkComponente) values  (101, 2),
+	   													         (101, 3),
+                                                              (101, 4),
+                                                              (101, 5),
+                                                              (101, 6),
+                                                              (101, 7),
+                                                              (101, 8),
+                                                              (101, 9),
+                                                              (101, 10),
+                                                              (101, 11),
+                                                              (102, 3),
+                                                              (102, 4),
+                                                              (102, 5),
+                                                              (103, 6),
+                                                              (103, 7),
+                                                              (103, 8),
+                                                              (103, 9),
+                                                              (104, 10),
+                                                              (104, 11),
+                                                              (104, 12),
+                                                              (105, 1),
+                                                              (105, 2);
 -- INSERT INTO TORRE_COMPONENTES VALUES ();
 select * from Torre;
 select * from Torre_Componente;
 select * from Leitura;
 -- UPDATE Torre  SET SerialID = 'WERWTDS2' WHERE idTorre = 101;
+
+CREATE USER 'tecnico'@'localhost' IDENTIFIED BY 'urubu100';
+GRANT ALL ON Monitoll.* TO 'tecnico'@'localhost';
 
 
