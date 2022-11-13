fb3a7ecf595f054d76863ec0608e75b302f60900
pedrosptech2022 <pedro.cordeironeto@sptech.school>
2022-11-13 20:27:15 -0300

    att

--- ApiCapturaPy/Captura2.py
+++ ApiCapturaPy/Captura2.py
@@ -388,37 +388,7 @@ def InserirDadosMaquina(SerialID, OS, Maquina, Processador, Disco, RamSpeed):
 
     except pyodbc.Error as err:
         crsr.rollback()
-        print("Something went wrong: {}".format(err))
-
-
-
-
-cpuPercent = psutil.cpu_percent(interval = 1, percpu = True)
-ramTotal = round((psutil.virtual_memory() [0] / 10**9), 4)
-ramUso = round((psutil.virtual_memory() [3] / 10**9), 4)
-ramUsoPercent = round((psutil.virtual_memory() [2]), 1)
-discoTotal = round((psutil.disk_usage("/")[0] / 10**12), 3)
-discoUso = round((psutil.disk_usage("/")[1] / 10**12), 3)
-discoLivre = round((psutil.disk_usage("/")[2] / 10**12), 3)
-discoPercent = psutil.disk_usage("/")[3]
-pacoEnv = round((psutil.net_io_counters(pernic=False, nowrap=True) [2] / 1024), 2)
-pacoRec = round((psutil.net_io_counters(pernic=False, nowrap=True) [3] / 1024), 2)
-pacoPerd = [pacoEnv, pacoRec]
-
-
-
-def InserirLocal():
-
-    try: 
-        insertquery = "INSERT INTO Leitura VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        values = [cpuPercent, ramTotal, ramUso, ramUsoPercent, discoTotal, discoUso, discoLivre, discoPercent, pacoEnv, pacoRec ,pacoPerd]
-        cursor.execute(insertquery, values)
-    except Error as e:
-        print("Error while connecting to MySQL", e)
-
-         
-
-            
+        print("Something went wrong: {}".format(err))            
 
 while True:
     Conexao1()
