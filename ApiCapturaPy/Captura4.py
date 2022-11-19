0ee707e6ebba03a82ef99f2d7d824646b9072dbd
victor lage <victor.lage@sptech.school>
2022-11-19 19:39:59 -0300

    a

--- ApiCapturaPy/Captura4.py
+++ ApiCapturaPy/Captura4.py
@@ -21,7 +21,7 @@ from json import loads
 def ConectarBancoLocal():
     try:
         conn = mysql.connector.connect(
-            host='172.17.0.2',
+            host='172.17.0.3',
             user='root',
             password='123',
             database='MoniToll'
