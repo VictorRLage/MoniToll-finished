d006c18772b785010695703f395267f264592294
victor lage <victor.lage@sptech.school>
2022-11-19 21:45:38 -0300

    a

--- ApiCapturaPy/Captura4.py
+++ ApiCapturaPy/Captura4.py
@@ -145,7 +145,7 @@ def ValidarLogin(email,senha):
         global usuario
         usuario = crsr.fetchall()
         print("Login efetuado com sucesso")
-        print(usuario)
+        print(usuario[1])
 
 
 
