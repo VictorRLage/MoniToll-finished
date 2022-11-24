5a6ae882120889efcbfe1e98aca2f718c4490fae
victor lage <victor.lage@sptech.school>
2022-11-24 11:29:01 -0300

    att

--- ApiCapturaPy/Captura4.py
+++ ApiCapturaPy/Captura4.py
@@ -115,7 +115,7 @@ def ConectarBancoLocal(login):
     if login:
         try:
             conn = mysql.connector.connect(
-                host='172.17.0.3',
+                host='172.17.0.2',
                 user='root',
                 password='123',
                 database='MoniToll'
