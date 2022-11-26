c82739b83c97d6a1f7beaf0eccd6860aa14e173a
victor lage <victor.lage@sptech.school>
2022-11-26 14:56:33 -0300

    a

--- ApiCapturaPy/Captura.py
+++ ApiCapturaPy/Captura.py
@@ -7,134 +7,10 @@ import datetime
 import functools
 import operator
 import pyodbc 
-from pyodbc import Error
 import textwrap
-import mysql.connector
-from mysql.connector import errorcode
-from asyncio import sleep
-from errno import errorcode
-from json import loads
 
+def Conexao():
 
-def Conexao2(cont, conn):
-    while True:
-        CpuPercent = psutil.cpu_percent(interval=1, percpu=True)
-        QtdProcessadores = psutil.cpu_count(logical=True)
-        RamTotal = round((psutil.virtual_memory()[0] / 10**9), 4)
-        RamUso = round((psutil.virtual_memory()[3] / 10**9), 4)
-        PorcentUsoRam = round((psutil.virtual_memory()[2]), 1)
-        vmem = round((psutil.virtual_memory()[1] / 1024/1024/1024), 3)
-        DiscoRTotal = round((psutil.disk_usage('/')[0] / 10**12), 3)
-        UsoDiscoR = round((psutil.disk_usage('/')[1] / 10**12), 3)
-        LivreDiscoR = round((psutil.disk_usage('/')[2] / 10**12), 3)
-        PorcentDiscoR = psutil.disk_usage('/')[3]
-        BytesRec = round(psutil.net_io_counters()[1] / 10**6, 2)
-        BytesEnv = round(psutil.net_io_counters()[0] / 10**6, 2)
-        PacotesEnv = round((psutil.net_io_counters(
-            pernic=False, nowrap=True)[2] / 1024), 2)
-        PacotesRec = round((psutil.net_io_counters(
-            pernic=False, nowrap=True)[3] / 1024), 2)
-        contador = 0
-        for x in CpuPercent:
-            contador = contador + x
-            PorcentCPU = (round(contador/QtdProcessadores, 1))
-        vetor = [PorcentCPU, QtdProcessadores, RamTotal, RamUso, PorcentUsoRam,
-                 DiscoRTotal, UsoDiscoR, LivreDiscoR, PorcentDiscoR, PacotesEnv, PacotesRec, vmem, BytesRec, BytesEnv]
-        PorcPctperdidos = round((((vetor[10] - vetor[9])/vetor[10])*100), 1)
-        datahora = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-
-        t = time.sleep(5)
-
-        def leitura(conn):
-            cursor = conn.cursor()
-
-            cursor.execute("INSERT INTO Leitura (dataHora, cpuPercent, ramTotal, ramUso, ramUsoPercent, discoTotal, discoUso, discoLivre, discoPercent, pacoEnv, pacoRec ,pacoPerd) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-                           (datahora,PorcentCPU, RamTotal, RamUso, PorcentUsoRam, DiscoRTotal, UsoDiscoR, LivreDiscoR,
-                            PorcentDiscoR, PacotesEnv, PacotesRec, PorcPctperdidos))
-            conn.commit()
-
-            print("Inserindo dados no banco de dados!")
-
-        if cont > cont: # Este contador realiza apenas 4 leitura e para, não sendo necessário dar ctrl+c para para o serviço de captura
-            break
-        else:
-            cont += 1
-
-        leitura(conn)
-
-try:
-    conn = mysql.connector.connect(
-        host='172.17.0.2',
-        user='root',
-        password='123',
-        database='MoniToll'
-    )
-    print("Conexão com o Banco de Dados MySQL efetuada com sucesso!")
-
-    cont = 0
-
-# Validações de Erro:
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Algo está errado com o Usuário do Banco ou a Senha.")
-        time.sleep(10)
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("O banco de dados direcionado não existe.")
-        time.sleep(10)
-    else:
-        print(err)
-        time.sleep(10)
-
-
-def ValidacaoLogin():
-
-    records = u_email = input('Seu e-mail: ')
-    records2 = u_senha = input('Sua senha: ')
-                    
-    try:
-        crsr.execute('''
-    SELECT Nome FROM Usuario WHERE Email = ? and Senha = ?
-    ''',records, records2)
-        # Executando comando SQL
-        print("Fazendo login...")
-        usuario = crsr.fetchone()
-
-    except pyodbc.Error as err:
-        print("Something went wrong: {}".format(err))
-    
-    if usuario is not None:
-        def convertTuple(tup):
-            str = ''
-            for item in tup:
-                str = str + item
-            return str
-        str_usuario = convertTuple(usuario)
-        print('Olá,',str_usuario,'!')
-                    
-        try:
-            crsr.execute('''
-        SELECT fkEmpresa FROM Usuario WHERE Email = ? and Senha = ?
-        ''',u_email, u_senha)
-            # Executando comando SQL
-            global fkEmpresa
-            fkEmpresa = crsr.fetchone()
-            global int_fkEmpresa
-            int_fkEmpresa = sum(fkEmpresa)
-            print('fkEmpresa:', fkEmpresa)
-
-        except pyodbc.Error as err:
-            print("Something went wrong: {}".format(err))
-
-        SelectIdTorres(fkEmpresa)
-
-    else:
-        print('Email ou senha incoretos')
-        ValidacaoLogin()
-
-
-def Conexao1():
-
-    try:
         # variaveis de conexao
         driver ='{ODBC Driver 18 for SQL Server}'
         server_name = 'montioll'
@@ -164,17 +40,57 @@ def Conexao1():
 
         global crsr
         crsr = cnxn.cursor()
-        print("Conectado ao banco de dados da Nuvem:")
-        cnxn.close()
+        print("Conectado ao banco de dados:")
+
+
+def ValidacaoLogin():
+
+    records = u_email = input('Seu e-mail: ')
+    records2 = u_senha = input('Sua senha: ')
+                    
+    try:
+        crsr.execute('''
+    SELECT Nome FROM Usuario WHERE Email = ? and Senha = ?
+    ''',records, records2)
+        # Executando comando SQL
+        print("Fazendo login...")
+        usuario = crsr.fetchone()
+        
+
+    except pyodbc.Error as err:
+        print("Something went wrong: {}".format(err))
+    
+    if usuario is not None:
+        def convertTuple(tup):
+            str = ''
+            for item in tup:
+                str = str + item
+            return str
+        str_usuario = convertTuple(usuario)
+        print('Olá,',str_usuario,'!')
+
+                    
+        try:
+            crsr.execute('''
+        SELECT fkEmpresa FROM Usuario WHERE Email = ? and Senha = ?
+        ''',u_email, u_senha)
+            # Executando comando SQL
+            global fkEmpresa
+            fkEmpresa = crsr.fetchone()
+            global int_fkEmpresa
+            int_fkEmpresa = sum(fkEmpresa)
+            print('fkEmpresa:', fkEmpresa)
+        
+
+        except pyodbc.Error as err:
+            print("Something went wrong: {}".format(err))
+
+        SelectIdTorres(fkEmpresa)
+
+
+    else:
+        print('Email ou senha incoretos')
         ValidacaoLogin()
-        VerificarDadosMaquina(idTorre)
-
-    except pyodbc.Error as ex:
-        print("{c} não conexão com o banco".format(c=connection_string))    
-        Conexao2(cont, conn)
-
-
-
 
 def EscolherTorres(idTorres):
     maquinas = numpy.asarray(idTorres)
@@ -243,7 +159,6 @@ global strip2_RamAtual
 strip2_RamAtual = strip_RamAtual.strip('\n')
 
 
-
 def teste():
         print("Inserindo leitura no banco...")
         datahora = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
@@ -279,7 +194,6 @@ def teste():
         except pyodbc.Error as err:
             cnxn.rollback()
             print("Something went wrong: {}".format(err))
-
 
             
 
@@ -349,8 +263,6 @@ def InserindoLeitura():
                 print(strNome + " = " + strCodigo)
                 teste()
 
-
-
 def VerificarDadosMaquina(idTorre):
                     
     try:
@@ -373,8 +285,6 @@ def VerificarDadosMaquina(idTorre):
         InserirDadosMaquina(strip_SerialIdAtual, strip3_OsAtual, strip3_MaquinaAtual, strip2_ProcessadorAtual, strip2_DiscoAtual, strip2_RamAtual)
 
 
-
-
 def InserirDadosMaquina(SerialID, OS, Maquina, Processador, Disco, RamSpeed):
     
     try:
@@ -388,9 +298,10 @@ def InserirDadosMaquina(SerialID, OS, Maquina, Processador, Disco, RamSpeed):
 
     except pyodbc.Error as err:
         crsr.rollback()
-        print("Something went wrong: {}".format(err))            
+        print("Something went wrong: {}".format(err))
 
+Conexao()
+ValidacaoLogin()
 while True:
-    Conexao1()
-    ValidacaoLogin()
+    VerificarDadosMaquina(idTorre)
     time.sleep(5)
