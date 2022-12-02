47ec4dcf94ded71f4d2d4c94fc2cc9ef6af34ce9
RenatoTierno <renato.tierno@sptech.school>
2022-12-02 17:14:08 -0300

    Pipefy Python

--- All/Python/Captura+Alerta.py
+++ All/Python/Captura+Alerta.py
@@ -459,21 +459,21 @@ def VerificarMetricas(Leitura,idComponente,idTorre):
             alertar = True
         else:
             print('Nenhum alerta emitido')
-    alertas(frase,componente,Leitura,idTorre,alertar)
+    alertas(frase,componente,Leitura,Leitura,idTorre,alertar)
 
 def alertas(frase,componente,Leitura,idTorre,alertar):
     if alertar:
-    #     url = "https://api.pipefy.com/graphql"
+        url = "https://api.pipefy.com/graphql"
 
-    #     payload = {"query": "{"mutation { createCard(input: { pipe_id:\"302621694\" fields_attributes:[ {field_id: "nome_da_empresa", field_value: "${nomeEmp}"},{field_id: "descri_o_do_alerta", field_value: "${componente}"},{field_id: "m_tricas", field_value: " ${frase}: ${metrica}"}]})}"}
-    #     headers = {
-    #         "Authorizarion": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDIwOTE4NzAsImVtYWlsIjoicmVuYXRvLnRpZXJub0BzcHRlY2guc2Nob29sIiwiYXBwbGljYXRpb24iOjMwMDIwMDc5OX19.u1OD3vfD6im7FYV9owyD6kVPdstkeU3_1tX-WJdZz0Pf5VM8QZ2VEO6vEye9ht82VD7t2bnBqMwtuWywW0rjEg"
-    #         "Content-Type": "application/json"
-    #     }
+        payload = {f"query": "mutation { createCard(input: { pipe_id:\"302621694\" fields_attributes:[ {field_id: \"nome_da_empresa\", field_value: \"{nomeEmp}\"},{field_id: \"descri_o_do_alerta\", field_value: \"{componente}\"},{field_id: \"m_tricas\", field_value: \"{frase}: {Leitura}\"}]})}"}
+        headers = {
+            "Authorizarion": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDIwOTE4NzAsImVtYWlsIjoicmVuYXRvLnRpZXJub0BzcHRlY2guc2Nob29sIiwiYXBwbGljYXRpb24iOjMwMDIwMDc5OX19.u1OD3vfD6im7FYV9owyD6kVPdstkeU3_1tX-WJdZz0Pf5VM8QZ2VEO6vEye9ht82VD7t2bnBqMwtuWywW0rjEg",
+            "Content-Type": "application/json"
+        }
 
-    # response = requests.post(url, json=payload, headers=headers)
+    response = requests.post(url, json=payload, headers=headers)
 
-    # print(response.text)
+    print(response.text)
     
         
 
