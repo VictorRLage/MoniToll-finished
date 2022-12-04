30a15a4b083ea09a37b97910f5069f0597c3385d
pedrosptech2022 <pedro.cordeironeto@sptech.school>
2022-12-04 00:40:14 -0300

    deixando só o necessario no projeto individual do pedro

--- Projetos_Individuais/Pedrinho/src/routes/medidas.js
+++ Projetos_Individuais/Pedrinho/src/routes/medidas.js
@@ -1,31 +1,3 @@
-var express = require("express");
-var router = express.Router();
-
-var medidaController = require("../controllers/medidaController");
-
-router.get("/ultimas/:idAquario", function (req, res) {
-    medidaController.buscarUltimasMedidas(req, res);
-});
-
-router.get("/tempo-real/:fkTorre/:fkComponente", function (req, res) {
-    medidaController.buscarMedidasEmTempoReal(req, res);
-})
-
-router.get("/PorcentagemCPU/:fkTorre", function (req, res) {
-    medidaController.buscarPorcentagemCPU(req, res);
-})
-
-router.get("/PorcentagemRAM/:fkTorre", function (req, res) {
-    medidaController.buscarPorcentagemRAM(req, res);
-})
-
-router.get("/PorcentagemDisco/:fkTorre", function (req, res) {
-    medidaController.buscarPorcentagemDisco(req, res);
-})
-
-router.get("/PorcentagemPercaPacotes/:fkTorre", function (req, res) {
-    medidaController.buscarPorcentagemPercaPacotes(req, res);
-})
 
 router.get("/Temperatura/:fkTorre", function (req, res) {
     medidaController.buscarTemperatura(req, res);
@@ -38,14 +10,3 @@ router.get("/PlacaMae/:fkTorre", function (req, res) {
 router.get("/Desempenho/:fkTorre", function (req, res) {
     medidaController.buscarPlacaMae(req, res);
 })
-
-router.get("/DataHora/:fkTorre", function (req, res) {
-    medidaController.buscarDataHora(req, res);
-})
-
-router.get("/Registro/:fkTorre/:nmrComponentes", function (req, res) {
-    medidaController.buscarRegistro(req, res);
-})
-
-
-module.exports = router;
