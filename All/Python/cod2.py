69267d359571e8d80f7a105df2e9df68b72d3c4a
bluzzcola <gabriela.silva@sptech.school>
2022-12-07 11:25:21 -0300

    final back

--- All/Python/cod2.py
+++ All/Python/cod2.py
@@ -9,9 +9,10 @@ import urllib.parse
 from urllib.parse import quote
 import textwrap
 import pyodbc
+import time
+import numpy
 
-
-accuweatherAPIKey = "k2rEt6SyATPfZeL53iL5AOw9l3Rfk6Lo"
+accuweatherAPIKey = "yEXfpNKSOMfMVf69A3msVe4g9XGUAmAq"
 mapboxToken = "pk.eyJ1IjoiYXJvbm5pIiwiYSI6ImNsYTczOGZxejBqbnczdmxkNjNuN3Q1bTIifQ.0tBJGc81IZLQopZnQ--cPg"
 # my_date = datetime.today() # if date is 01/01/2018
 # year, week_num, day_of_week = my_date.isocalendar()
@@ -112,15 +113,29 @@ def pegarPrevisao5Dias(codigoLocal):
 
     ##Inicio do cod
 ##Conexão com o banco
-try:
+def Login(conectado):
+
+    if conectado == 1 or conectado == 3:
+        print("Bem vindo ao Grenn Light!")
+        print("Login")
+        u_email = input('Seu e-mail: ')
+        u_senha = input('Sua senha: ')
+        ValidarLogin(u_email, u_senha)
+    elif conectado == 0:
+        print("Sem conexão com a internet.")
+
+def ConectarBancoAzure(nmr, login):
+
+    try:
         # variaveis de conexao
-        driver ='{ODBC Driver 18 for SQL Server}'
+        driver = '{ODBC Driver 18 for SQL Server}'
         server_name = 'montioll'
         database_name = 'Monitoll'
-        server = '{server_name}.database.windows.net,1433'.format(server_name=server_name)
+        server = '{server_name}.database.windows.net,1433'.format(
+            server_name=server_name)
         username = 'Monitoll'
         password = 'Grupo7@123'
-        # definindo banco url 
+        # definindo banco url
         connection_string = textwrap.dedent('''
         Driver={driver};
         Server={server};
@@ -136,39 +151,22 @@ try:
             database=database_name,
             username=username,
             password=password
-        )) 
+        ))
 
-        cnxn:pyodbc.Connection = pyodbc.connect(connection_string) 
-
+        cnxn: pyodbc.Connection = pyodbc.connect(connection_string)
         global crsr
         crsr = cnxn.cursor()
-        print("Conectado ao banco de dados:")
-except pyodbc.Error as err:
-            print(err)
-            print('erro no envio')
-# config = {
-#     'host': 'localhost',
-#     'user': 'root',
-#     'password': '#Gf232623',
-#     'database': 'projeto_pi'
-# }
+        print("Conectado ao banco de dados da Nuvem")
+        global conectado
+        conectado = nmr
 
-# try:
-#     conn = mysql.connector.connect(**config)
-#     print("Connection established")
-# except mysql.connector.Error as err:
-#   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-#     print("Something is wrong with the user name or password")
-#   elif err.errno == errorcode.ER_BAD_DB_ERROR:
-#     print("Database does not exist")
-#   else:
-#     print(err)
-# else:
-#   cursor = conn.cursor()
+    except pyodbc.Error as ex:
+        print("Conexão com a Azure perdida")
+        print(ex)
+        conectado = 0
 
-#cod para envio dos dados da
-# dataHora = datetime()
-# print(dataHora)
+
+
 
 def pesquisarLocal(local):
     _local = urllib.parse.quote(local)
@@ -187,58 +185,160 @@ def pesquisarLocal(local):
         except:
             print('Erro na pesquisa de local.')
 
-try:
-    select = ('SELECT idTorre, localização FROM torre where idTorre = 171;')
-    crsr.execute(select)
-    resultado = crsr.fetchall()
-    print(resultado)
-    for data in resultado:
-        print("Cidade: ", data[1])
-        coord = pesquisarLocal(data[1])
-        print(coord)
-        print(pegarCodigoLocal(coord['lat'],coord['long']))
-        local = pegarCodigoLocal(coord['lat'],coord['long'])
-        climaAtual = pegarTempoAgora(local['codigoLocal'], local['nomeLocal'])
-        print(climaAtual)
-        try:
-            tc = infoClima['textoClima']
-            temp = infoClima['temperatura']
-            dt = data[0]
-            crsr.execute('''
-            INSERT INTO TempLocal(textoClima,temperatura,fkTorre) VALUES(?,?,?)
-            ''',tc,temp,dt)
-            crsr.commit()
-            print('Dados enviados')
-        except pyodbc.Error as err:
-            print(err)
-            print('erro no envio')
-except pyodbc.Error as err:
-            print(err)
-            print('erro no envio')
+global v_login
+v_login = False
 
-try:
-    print(resultado)
-    for id in resultado:
-        coord = pesquisarLocal(id[1])
-        local = pegarCodigoLocal(coord['lat'],coord['long'])
-        semana = pegarPrevisao5Dias(local['codigoLocal'])
-        print(id)
-        try:
-            for i, data in enumerate(semana):
-                print(i, data)
-                dm = data['diaMes']
-                ds = data['diaSemana']
-                max = data['max']
-                min = data['min']
-                c = data['clima']
-                fk = id[0]
+
+# Validar login do cliente
+def ValidarLogin(email, senha):
+
+    try:
+        crsr.execute('''
+        SELECT Nome,fkEmpresa FROM Usuario WHERE Email = ? and Senha = ?
+        ''', email, senha)
+        # Executando comando SQL
+        print("Fazendo login...")
+        global usuario
+        usuario = crsr.fetchall()
+        print("Login efetuado com sucesso")
+        u_usuario = usuario[0]
+        print(f'Bem vindo {u_usuario[0]}!')
+        global fkEmpresa
+        fkEmpresa = u_usuario[1]
+        global v_login
+        v_login = True
+        BuscarTorres(fkEmpresa)
+    except pyodbc.Error as err:
+        print("Something went wrong: {}".format(err))
+        print("Falha ao realizar login por favor tente novamente")
+        v_login = False
+
+# Buscar as torres cadastradas na empresa do cliente
+def BuscarTorres(fkEmpresa):
+
+    try:
+        crsr.execute('''
+    SELECT idTorre FROM Torre WHERE fkEmpresa = ?
+    ''', fkEmpresa)
+        # Executando comando SQL)
+        idTorres = crsr.fetchall()
+        BuscarNomeEmp(fkEmpresa)
+        EscolherTorres(idTorres)
+
+    except pyodbc.Error as err:
+        print("Something went wrong: {}".format(err))
+
+def BuscarNomeEmp(fkEmpresa):
+    try:
+        crsr.execute('''
+    SELECT Nome FROM Empresa WHERE idEmpresa = ?
+    ''', fkEmpresa)
+        # Executando comando SQL)
+        nomeEmpFetch = crsr.fetchone()
+        global nomeEmp
+        nomeEmp = nomeEmpFetch[0]
+
+    except pyodbc.Error as err:
+        print("Something went wrong: {}".format(err))
+
+def EscolherTorres(idTorres):
+    for x in idTorres:
+        print('Maquina:', x[0])
+    global idTorre
+    idTorre = input('Qual é esta maquina? ')
+    VerificarDadosMaquina(idTorre)
+
+# Validar se as maquinas possuen seus dados (serialID, SO, Nome, Velicidade da RAM, Marca do Disco) já cadastrados
+def VerificarDadosMaquina(idTorre):
+
+    try:
+        crsr.execute('''
+        SELECT SerialID FROM Torre WHERE idTorre = ?
+        ''', idTorre)
+        # Executando comando SQL
+        print("Verificando dados da torre...")
+        SerialIdBanco = crsr.fetchone()
+
+    except pyodbc.Error as err:
+        print("Something went wrong: {}".format(err))
+
+    if SerialIdBanco[0] != '':
+        print("A torre possui dados cadastrados")
+        global v_login
+        ConectarBancoAzure(3, v_login)
+    else:
+        print("A torre não possui dados")
+
+
+def EscolherTorre():
+    try:
+            crsr.execute('''
+            SELECT idTorre, localização FROM torre where idTorre = ?
+            ''', idTorre)
+            global resultado
+            resultado = crsr.fetchall()
+            print(resultado)
+    except pyodbc.Error as err:
+                    print(err)
+                    print('erro no envio')
+
+def tempLocal():
+    try:
+        for data in resultado:
+            print("Cidade: ", data[1])
+            coord = pesquisarLocal(data[1])
+            print(coord)
+            print(pegarCodigoLocal(coord['lat'],coord['long']))
+            local = pegarCodigoLocal(coord['lat'],coord['long'])
+            climaAtual = pegarTempoAgora(local['codigoLocal'], local['nomeLocal'])
+            print(climaAtual)
+            try:
+                tc = infoClima['textoClima']
+                temp = infoClima['temperatura']
+                dt = data[0]
                 crsr.execute('''
-                INSERT INTO TempSemana(diaMes, diaSemana, climaMax, climaMin, climaTexto, fkTorre) VALUES(?,?,?,?,?,?)
-                ''',dm,ds,max,min,c,fk)
-                cnxn.commit()
-        except pyodbc.Error as err:
-            print(err)
-            print('erro no envio')
-except pyodbc.Error as err:
-            print(err)
-            print('erro no envio')
+                INSERT INTO TempLocal(textoClima,temperatura,fkTorre) VALUES(?,?,?)
+                ''',tc,temp,dt)
+                crsr.commit()
+                print('Dados enviados')
+            except pyodbc.Error as err:
+                print(err)
+                print('erro no envio')
+    except pyodbc.Error as err:
+                print(err)
+                print('erro no envio')
+
+def tempSemana():
+    try:
+        for id in resultado:
+            coord = pesquisarLocal(id[1])
+            local = pegarCodigoLocal(coord['lat'],coord['long'])
+            semana = pegarPrevisao5Dias(local['codigoLocal'])
+            print(id)
+            try:
+                for i, data in enumerate(semana):
+                    print(i, data)
+                    dm = data['diaMes']
+                    ds = data['diaSemana']
+                    max = data['max']
+                    min = data['min']
+                    c = data['clima']
+                    crsr.execute('''
+                    INSERT INTO TempSemana(diaMes, diaSemana, climaMax, climaMin, climaTexto, fkTorre) VALUES(?,?,?,?,?,?)
+                    ''',dm,ds,max,min,c,idTorre)
+                    crsr.commit()
+            except pyodbc.Error as err:
+                print(err)
+                print('erro no envio')
+    except pyodbc.Error as err:
+                print(err)
+                print('erro no envio')
+
+ConectarBancoAzure(1, v_login)
+Login(conectado)
+while(True):
+    ConectarBancoAzure(3,v_login)
+    EscolherTorre()
+    tempLocal()
+    time.sleep(10)
+    tempSemana()
